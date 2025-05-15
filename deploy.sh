9325b62a4dc9ead08f7263b03eb64579ca9eed70
MohammadJabaren <mohammad.eek.99@gmail.com>
2025-05-15 21:12:22 +0300

    Fix Path Of Requirements

--- deploy.sh
+++ deploy.sh
@@ -27,8 +27,6 @@ fi
 # === 3. Activate the virtual environment ===
 source "$VENV_DIR/bin/activate"
 
-# === 4. Install dependencies ===
-pip install --upgrade pip
 echo " Python requirements installed."
 
 # === 5. Ensure .env file contains correct secrets ===
