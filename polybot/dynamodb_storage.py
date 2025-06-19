6e2aae5ddcfe535deee77c2bbdac461a90b27c74
MohammadJabaren <mohammad.eek.99@gmail.com>
2025-06-19 18:29:56 +0300

    add the region of dynamodb

--- polybot/dynamodb_storage.py
+++ polybot/dynamodb_storage.py
@@ -5,11 +5,15 @@ import os
 load_dotenv()
 PREDICTION_SESSIONS = os.getenv("PREDICTION_SESSIONS")
 DETECTION_OBJECTS = os.getenv("DETECTION_OBJECTS")
+
 class DynamoDBStorage:
     def __init__(self):
-        dynamodb = boto3.resource("dynamodb")  # Use default credentials
-        self.session_table = dynamodb.Table(PREDICTION_SESSIONS)
-        self.objects_table = dynamodb.Table(DETECTION_OBJECTS)
+        region = os.getenv("AWS_REGION", "us-west-1")  # default to us-west-1 if not set
+        if not region:
+            raise ValueError("Missing AWS_REGION environment variable")
+        self.dynamodb = boto3.resource('dynamodb', region_name=region)
+        self.session_table = self.dynamodb.Table(PREDICTION_SESSIONS)
+        self.objects_table = self.dynamodb.Table(DETECTION_OBJECTS)
 
     def get_prediction(self, uid: str) -> Dict:
         session = self.session_table.get_item(Key={"uid": uid}).get("Item")
